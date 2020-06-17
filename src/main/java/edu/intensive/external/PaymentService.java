922d6ed629c9d4de81c60c3359a1e631732fe7c7
SKCC <SKCC@SKCC18D00080.SKCC.NET>
2020-06-17 16:38:45 +0900

    update feign url

--- src/main/java/edu/intensive/external/PaymentService.java
+++ src/main/java/edu/intensive/external/PaymentService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="payment", url="localhost:8084")
+@FeignClient(name="payment", url="${feign.payment.url}")
 public interface PaymentService {
     @RequestMapping(method = RequestMethod.POST, path="/payments")
     public void enroll(@RequestBody Payment payment);
